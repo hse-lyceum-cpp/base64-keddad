5922ab0b5baa69981bbc038566d0a804993e4657
keddad <keddad@yandex,ru>
2019-12-22 11:08:06 +0300

    Temporary version to show compilation issues

--- base64.h
+++ base64.h
@@ -3,15 +3,10 @@
 #ifndef BASE64_KEDDAD_BASE64_H
 #define BASE64_KEDDAD_BASE64_H
 
-class Base64{
-public:
-    static std::string encode(const std::string &input);
-    static std::string decode(const std::string &input);
+namespace Base64 {
+    std::string encode(const std::string &input);
 
-private:
-    static void encode_chunk(char *chunk);
-    static void decode_chunk(char *chunk);
-    static void nullify_chunk(char *chunk);
+    std::string decode(const std::string &input);
 };
 
 #endif //BASE64_KEDDAD_BASE64_H
